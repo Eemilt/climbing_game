48c02e7b10a45d1450bc6668027fc43b14accc8a
alvaripi <50096369+alvaripi@users.noreply.github.com>
2021-02-26 11:45:36 +0200

    collision detection

--- scripts/main.js
+++ scripts/main.js
@@ -1,15 +1,18 @@
-/*
-Few changes from in-class:
-- remove the sine function for movement speed
-- stretched canvas to full screen in CSS
-- other small changes in CSS
-- removed the first 3 houses which were hard-coded there
-*/
+
 const SIZE=1000;
 const OBJECTS=[];
-let SPEED=0.001;     //vaikuttaa miten useasti objektin sijainti paivittyy eli toisinsanoen kuinka nopeasti ikkunat tulee vastaan
+
+let SPEED=0.000001;     //vaikuttaa miten useasti objektin sijainti paivittyy eli toisinsanoen kuinka nopeasti ikkunat tulee vastaan
 var rectX = 0.45;
 var rectY = 0.9;
+
+
+
+var window_x_position = 0.45;
+var window_y_position= 0.5
+var window_width = 0.15
+var window_height = 0.09
+
 
 function main(){
 	let canvas = document.getElementById("myCanvas")
@@ -43,45 +46,16 @@ function rect(ctx) {
 }
 
 
+
+
 var LEFT = false;
 var RIGHT = false;
 var UP = false;
 var DOWN = false;
 
-/**  turha ???
-function move() {
-	
-	if (RIGHT) { 
-		rectX = rectX+ 0.035;
-	}
-	if (LEFT) {
-		rectX = rectX-0.035;
-	}
-  if (UP) {
-    rectY = rectY - 0.035;
-  }
-  if (DOWN){
-    rectY = rectY + 0.035;
-  }
-	
-}
-
-*/
 
 function onkeydown(e) {
-  /*
-  if (RIGHT) { 
-		rectX = rectX+ 0.035;
-	}
-	if (LEFT) {
-		rectX = rectX-0.035;
-	}
-  if (UP) {
-    rectY = rectY - 0.035;
-  }
-  if (DOWN){
-    rectY = rectY + 0.035;
-  }*/
+
   
     if (e.keyCode == 39 && rectX<0.815) { 
       rectX = rectX+ 0.035;
@@ -99,7 +73,7 @@ function onkeydown(e) {
         rectY = rectY + 0.035;
       DOWN = true;
 	} //down arrow*/
-	console.log(" X arvo on:   "+rectX+" Y arvo on:  "+ rectY)
+
 	drawScene();
 }
 
@@ -107,54 +81,22 @@ function onkeydown(e) {
 
 window.addEventListener("keydown", onkeydown);
 
-////////////////////////////////////////////////////
-
-
-/*
-window.addEventListener("onkeydown", e => {
-  if(e.keyCode == 37) LEFT = true;
-	if(e.keyCode == 39) RIGHT = true;
-  // do something
-});
-
-window.addEventListener("keyup", e => {
-  if(e.keyCode == 37) LEFT = false;
-	if(e.keyCode == 39) RIGHT = false;
-  // do something
-});*/
-
-/*document.onkeydown = function(e) {
-	if(e.keyCode == 37) LEFT = true;
-	if(e.keyCode == 39) RIGHT = true;
-}
-
-document.onkeyup = function(e) {
-	if(e.keyCode == 37) LEFT = false;
-	if(e.keyCode == 39) RIGHT = false;
-}*/
-
 function animate(){
 	SPEED  += 0.0000002;
-  
-  // add objects to scene
-	let probability=Math.random(); //[0..1)		//spawnauksen sijaintitodennäköisyys
-	let probabilityxaxis = Math.random() //spawnattujen laatikoiden sivuittaissijainti
- 
-    if(probability>0.0){ // probability to generate SOME object, tällä hetkellä spawnataan niin usein kun pystyy ja rajoitetaan vain sillä ettei ikkunat saa tulla liian lähekkäin. Toisin sanoen ikkunoiden väli on koko ajan sama "sallitturaja" muuttuesta riippuen
-		properties={
+	
+	let probabilityxaxis = Math.random() * (0.708 - 0.131) +0.131; // arpoo ikkunan ruutuun
+
+	properties={
 		levels:1,
 		wallColor:"Brown",
 		roofColor:"BurlyWood"
       }
-        
-		let y=1; // between 0.5 and 1;
-		let size=(y*y/2)*0.5;
-		let sallitturaja = 1.4		//määrittää minkä korkeus arvon jälkeen uusi ikkuna saa spawnata
+	  
+		let sallitturaja = 0.15		//määrittää minkä korkeus arvon jälkeen uusi ikkuna saa spawnata, käytännössä ikkunoiden väli 
 		let mahtuuko = false;  		//tämä pistetään trueksi jos canvakseen mahtuu uusi ikkuna
-
 		if(OBJECTS.length>0){		//ehdotetaan että spawnaa ainakin jotain jos yhtään ikkunaa ei olekkaan liikenteessä else haarassa
 			//(OBJECTS[OBJECTS.length-1].location[1])
-			if(OBJECTS[OBJECTS.length-1].location[1]>sallitturaja){		//taulukon vika alkio on aina näemmä "ylin" ja sen korkeuden tarkastelu riittää päättämään voiko uuden ikkunan asettaa
+			if(OBJECTS[OBJECTS.length-1].window_y_position>sallitturaja){		//taulukon vika alkio on aina näemmä "ylin" ja sen korkeuden tarkastelu riittää päättämään voiko uuden ikkunan asettaa
 				mahtuuko= true;
 			}
 		}else{
@@ -162,34 +104,25 @@ function animate(){
 		}
 			
 		if(mahtuuko == true){		//tällä saa spawnattua vain yhden laatikon kerrallaan canvakseen
-			OBJECTS.push(new Windows([(probabilityxaxis*(0.75-0.22)+0.22)+size*0.5,y], size, properties));	
+			OBJECTS.push(new Windows(probabilityxaxis, -0.1, properties));
 		}
-
-		
-	}
 	
-
-    //console.log(" OBJECTS taulukon pituus on: "+OBJECTS.length);
-
-    
-	
-	// move objects in scene
 	for(let i=0;i<OBJECTS.length;i++){
-		OBJECTS[i].location[1]+=SPEED;
-		//console.log(i + " " + OBJECTS[i].location[0] +"taalla" + "rectX " + rectX);
-		//console.log(distance(OBJECTS[0].location[0],OBJECTS[0].location[1],rectX,rectY));
+		OBJECTS[i].window_y_position+=SPEED;
 
+			if(rectX + 0.05 +0.01 >= OBJECTS[i].window_x_position && rectX <= OBJECTS[i].window_x_position+window_width +0.01 && rectY + 0.05 +0.01  >= OBJECTS[i].window_y_position && rectY<=OBJECTS[i].window_y_position+window_height +0.01){
+					console.log("OSU PERK*LE!")
+			}
 		
-		//console.log(" location 1 arvo on " + OBJECTS[i].location[1]+" ja raja arvo on " + OBJECTS[i].scale*9.5); 	// tän avulla saa tausta väärin skaletettuna testailtua millon pitäs turhien "ikkunoiden" kadota
-		//console.log("arvon 0 sijainti on: " + OBJECTS[i].location[0] + " ja arvon 1 sijainti on: "+ OBJECTS[i].location[1])
-		if(OBJECTS[i].location[1]>OBJECTS[i].scale*9.5){				//object hävitetään taulukosta kun pysty muuttuja eli location[1] menee suuremmaksi kuin 2.375 eli pois näkymästä (en tiedä vaikuttaako selaimen skaalaus tms tähän valueen, en usko koska suhteellisia arvoja?)
+		if(OBJECTS[i].window_y_position>1){	//object hävitetään taulukosta kun pysty muuttuja eli location[1] menee suuremmaksi kuin 2.375 eli pois näkymästä (en tiedä vaikuttaako selaimen skaalaus tms tähän valueen, en usko koska suhteellisia arvoja?)
 			OBJECTS.splice(i,1); // removing 1 element at index i
-			console.log("------------ OBJECTS taulukosta poistettu alkio  -------------");
+			/*console.log("------------ OBJECTS taulukosta poistettu alkio  -------------");*/
 			i--;
 		}
+		
 	}
-    
-
+	
+	
   // sort to draw smaller objects first
 	OBJECTS.sort(function(a,b){
 		return a.scale-b.scale;
@@ -199,20 +132,17 @@ function animate(){
 	window.requestAnimationFrame(animate);			//tällä loopataan vissiin animaatio
 }
 
-function checkrepeat (){		//opelta jääny, en tiedä mitä tää tekee
+/* PYTHAGORAN voiaan kattoo esim ympyräobjektien etäisyyksiä
 
-
-}
-
-function distance(ikkuna_x, ikkuna_y, laatikko_x,laatikko_y ){
-	let x_etaisyys= laatikko_x-ikkuna_x;
-	let y_etaisyys = laatikko_y-ikkuna_y;
+function distance(item1_x, item1_y, item2_x,item2_y ){
+	let x_etaisyys= item2_x-item1_x;
+	let y_etaisyys = item2_y-item1_y;
 
 	return Math.sqrt(Math.pow(x_etaisyys,2) + Math.pow(y_etaisyys,2)) //Pythagoras
 }
+*/
 
 function drawScene(){
-	
 	let canvas = document.getElementById("myCanvas")
 	let ctx=canvas.getContext("2d");
 	
@@ -251,10 +181,12 @@ function getRandomColor(){
 	return "rgba("+red+","+green+","+blue+",1)";
 }
 
+
+
 class Windows{
-	constructor(loc, scale, properties){
-		this.location=loc;
-		this.scale=scale;
+	constructor(window_x_position, window_y_position, properties){
+		this.window_x_position=window_x_position;
+		this.window_y_position=window_y_position;
 		this.properties=properties;
 		this.angles=[];
 		for(let i=1;i<=this.properties.levels;i++){
@@ -263,59 +195,13 @@ class Windows{
 		}
 	}
 	draw(ctx){
-		ctx.beginPath();
-	
-		ctx.save();
-		ctx.translate(this.location[0],this.location[1]);
-		ctx.scale(this.scale,this.scale);
-		ctx.lineWidth=0.08;
+	ctx.beginPath();
+	ctx.lineWidth=0.02;
+	ctx.fillStyle="lightblue";
+	ctx.rect(this.window_x_position,this.window_y_position,window_width,window_height);
+	ctx.stroke();
+	ctx.fill();
 		
-		ctx.fillStyle="lightblue";		//ikkunan väri, tähän vois kokeilla saada vaikka jollain liukuvärjäyksellä lisää eloa. "kukkalauta" voisi olla tehtävissä lisäämällä ikkunan alaosaan esimerkiksi paksun viivan 		
-		ctx.beginPath();
-        ctx.rect(-0.9,-4.7,0.8,0.5);
-		ctx.stroke();
-		ctx.fill();
-		ctx.translate(0,-0.1);
-		ctx.restore();
-		
-	}
-}
-
-
-class Tree{					//tällä voisi spawnata satunnaisesti vaikka kiviä tms, atm turha/ei käytössä
-	constructor(loc, scale){
-		this.location=loc;
-		this.scale=scale;
-	}
-	draw(ctx){
-		ctx.beginPath();
-	
-		ctx.save();
-		ctx.translate(this.location[0],this.location[1]);
-		ctx.scale(this.scale,this.scale);
-		ctx.lineWidth=0.04;
-		
-		ctx.fillStyle="brown";
-		
-		//trunk
-		ctx.beginPath();
-		ctx.rect(-0.02,-0.1,0.04,0.1);
-		ctx.stroke();
-		ctx.fill();
-		
-		ctx.translate(0,-0.1);
-		
-		ctx.fillStyle="rgba(0,255,0,1)";
-		ctx.beginPath();
-		ctx.moveTo(-0.2,-0.0);
-		ctx.lineTo(+0.2,-0.0);
-		ctx.lineTo(+0.0,-0.9);
-		ctx.lineTo(-0.2,-0.0);
-		ctx.closePath();
-		ctx.stroke();
-		ctx.fill();
-		
-		ctx.restore();
 	}
 }
 
