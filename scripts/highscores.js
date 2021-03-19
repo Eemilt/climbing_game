80a63868d8c674e53f60778ea157da5ff4b034ca
erikhamalainen <50096393+erikhamalainen@users.noreply.github.com>
2021-03-19 13:21:33 +0200

    idea
    idea addition

--- scripts/highscores.js
+++ scripts/highscores.js
@@ -1,3 +1,5 @@
+//idea for high score code from https://github.com/jamesqquick/Build-A-Quiz-App-With-HTML-CSS-and-JavaScript
+
 const highScoresList = document.getElementById("highScoresList");
 const highScores = JSON.parse(localStorage.getItem("highScores")) || [];
 
